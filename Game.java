cc0f0fb30f1d01961c3b408ba112daec4f14c896
DivyaKishanPatel <pate5903@sheridancollege.ca>
2024-11-30 23:17:32 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -11,48 +11,129 @@ import java.util.ArrayList;
  * The class that models your game. You should create a more specific child of this class and instantiate the methods
  * given.
  *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * @author Hinal
+ * @author Dhruv 
+ * Date: Nov 30, 2024
  */
-public abstract class Game {
 
-    private final String name;//the title of the game
-    private ArrayList<Player> players;// the players of the game
+import java.util.Scanner;
 
-    public Game(String name) {
-        this.name = name;
-        players = new ArrayList();
+public class GoFishGame {
+    private ArrayList<Player> players;
+    private GroupOfCards deck;
+    private int currentPlayerIndex;
+
+    public GoFishGame() {
+        players = new ArrayList<>();
+        deck = new GroupOfCards(52);
+        deck.shuffle();
+        currentPlayerIndex = 0;
     }
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
+    public void addPlayer(Player player) {
+        players.add(player);
     }
 
-    /**
-     * @return the players of this game
-     */
-    public ArrayList<Player> getPlayers() {
-        return players;
+    public void startGame() {
+        // Deal cards to players
+        for (Player player : players) {
+            for (int i = 0; i < 7; i++) { // Dealing 7 cards to each player
+                player.addCard(deck.drawCard());
+            }
+        }
+        play();
     }
 
-    /**
-     * @param players the players of this game
-     */
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
+    public void play() {
+        Scanner scanner = new Scanner(System.in);
+        while (deck.getSize() > 0) {
+            Player currentPlayer = players.get(currentPlayerIndex);
+            System.out.println(currentPlayer.getName() + "'s turn!");
+            System.out.println("Your hand: " + currentPlayer.getHand());
+
+            // Ask another player for a card
+            System.out.print("Enter rank to request: ");
+            String requestedRank = scanner.nextLine();
+
+            // Find the next player to ask
+            Player targetPlayer = players.get((currentPlayerIndex + 1) % players.size());
+
+          if (targetPlayer.hasCard(requestedRank)) {
+    System.out.println(targetPlayer.getName() + " has the " + requestedRank + "!");
+    // Transfer the card
+    Card card = null;
+    // Iterate over the cards to find the matching card
+    for (Card c : targetPlayer.getHand()) {
+        if (c instanceof GoFishCard && ((GoFishCard) c).getRank().equals(requestedRank)) {
+            card = c;
+            break; // Exit the loop once the card is found
+        }
+    }
+    if (card != null) {
+        targetPlayer.removeCard(card);
+        currentPlayer.addCard(card);
+    }
+} else {
+    System.out.println(targetPlayer.getName() + " does not have it. Drawing a card.");
+    currentPlayer.addCard(deck.drawCard());
+}
+
+
+            // Check for matches
+            checkForMatches(currentPlayer);
+
+            // End turn
+            currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
+        }
+
+        declareWinner();
     }
 
-    /**
-     * Play the game. This might be one method or many method calls depending on your game.
-     */
-    public abstract void play();
+ private void checkForMatches(Player player) {
+    ArrayList<Card> hand = player.getHand();
+    ArrayList<Card> toRemove = new ArrayList<>();
+    ArrayList<String> ranksChecked = new ArrayList<>();
 
-    /**
-     * When the game is over, use this method to declare and display a winning player.
-     */
-    public abstract void declareWinner();
+    // Check for sets of 4 cards with the same rank
+    for (Card card : hand) {
+        if (card instanceof GoFishCard goFishCard) {
+            String rank = goFishCard.getRank();
+            if (!ranksChecked.contains(rank)) {
+                ranksChecked.add(rank);
+                int count = 0;
+                for (Card c : hand) {
+                    if (c instanceof GoFishCard && ((GoFishCard) c).getRank().equals(rank)) {
+                        count++;
+                    }
+                }
 
-}//end class
+                if (count == 4) {
+                    System.out.println(player.getName() + " has a set of four " + rank + "!");
+                    // Remove the 4 cards from the hand and add them to collected sets
+                    for (Card c : hand) {
+                        if (c instanceof GoFishCard && ((GoFishCard) c).getRank().equals(rank)) {
+                            toRemove.add(c);
+                        }
+                    }
+                    player.collectSet(toRemove.get(0));  // Assuming all cards are the same rank
+                    hand.removeAll(toRemove);
+                }
+            }
+        }
+    }
+}
+
+    private void declareWinner() {
+        // Declare winner based on the most sets collected
+        Player winner = null;
+        int maxSets = 0;
+        for (Player player : players) {
+            int setCount = player.getCollectedSets().size();
+            if (setCount > maxSets) {
+                maxSets = setCount;
+                winner = player;
+            }
+        }
+        System.out.println(winner.getName() + " wins with " + maxSets + " sets of cards!");
+    }
+}
