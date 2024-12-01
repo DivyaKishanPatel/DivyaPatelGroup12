9c33b541e253424a781a8dde8529906ad70cc97e
DivyaKishanPatel <pate5903@sheridancollege.ca>
2024-11-30 23:25:57 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+package gofishcard;
+
+/**
+ *
+ * @author divya nov 2024
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        GoFishGame game = new GoFishGame();
+
+        // Create and add players
+        game.addPlayer(new Player("Player 1"));
+        game.addPlayer(new Player("Player 2"));
+
+        // Start the game
+        game.startGame();
+    }
+}
