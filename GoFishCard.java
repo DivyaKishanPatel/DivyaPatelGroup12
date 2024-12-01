a00e5f26d0f753b1b6d169178fb40efbc1a49eef
DivyaKishanPatel <pate5903@sheridancollege.ca>
2024-11-30 23:27:37 -0500

    Create GoFishCard.java

---
+++ GoFishCard.java
@@ -0,0 +1,31 @@
+package gofishcard;
+
+/**
+ *
+ * @author Dhruv
+ * @author Kriti Nov 29, 2024
+ */
+public class GoFishCard extends Card {
+    private String rank;
+    private String suit;
+
+    public GoFishCard(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+  
+}
