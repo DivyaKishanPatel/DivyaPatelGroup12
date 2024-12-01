d5788d920999e689371b3370b4cbf5fe2d9b4273
DivyaKishanPatel <pate5903@sheridancollege.ca>
2024-11-30 23:08:13 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -3,23 +3,17 @@
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
  */
-package ca.sheridancollege.project;
+package gofishcard;
 
 /**
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
- * @author dancye
+ * @author divya
  */
 public abstract class Card {
-    //default modifier for child classes
-
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
+    // Abstract method to represent a card as a string (could be for any card game)
     @Override
     public abstract String toString();
-
 }
+
